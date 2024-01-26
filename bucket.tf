4c221a1d2a2f5895f98e2ca7769c5a821a034560
srikanth5572 <155812214+srikanth5572@users.noreply.github.com>
2024-01-26 20:07:54 +0530

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -2,8 +2,8 @@
 provider "aws" {
   region = "ap-south-1"
 }
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
+resource "aws_s3_bucket" "sampe1" {
+  bucket = "srikanth-image"
 
   tags = {
     Name        = "My bucket"
